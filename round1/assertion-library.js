5fcf297504679e9b986ad409d5994cd3764a646f
sirwan <sir1afifi@gmail.com>
2019-11-24 16:30:44 +0330

    Add async versions

--- round1/assertion-library.js
+++ round1/assertion-library.js
@@ -1,13 +1,13 @@
-const { sum, subtract } = require("./math");
+const { sumAsync, subtractAsync } = require("./math");
 
-test("sum adds numbers", () => {
-    const result = sum(3, 7);
+test("sum adds numbers", async () => {
+    const result = await sumAsync(3, 7);
     const expected = 10;
     expect(result).toBe(expected);
 });
 
 test("subtract subtracts numbers", () => {
-    const result = subtract(7, 3);
+    const result = await subtractAsync(7, 3);
     const expected = 4;
     expect(result).toBe(expected);
 });
@@ -16,9 +16,9 @@ test("subtract subtracts numbers", () => {
 
 
 
-function test(title, callback) {
+async function test(title, callback) {
     try {
-        callback();
+        await callback();
         console.log(`X ${title}`);
     } catch (error) {
         console.log(`X ${title}`);
