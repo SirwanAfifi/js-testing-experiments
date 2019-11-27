26c9a89954524419721b7865bd09695b2a5fee07
sirwan <sir1afifi@gmail.com>
2019-11-27 09:31:04 +0330

    Make prettier part of script

--- static-testing-tools/src/example.js
+++ static-testing-tools/src/example.js
@@ -1,11 +1,11 @@
-const myName = 'Freddy';
-typeof myName === 'string';
+const myName = "Freddy";
+typeof myName === "string";
 
-if (!('serviceWorker' in navigator)) {
-    // you have an old browser :-(
+if (!("serviceWorker" in navigator)) {
+  // you have an old browser :-(
 }
 
-const greeting = 'hello';
+const greeting = "hello";
 console.log(`${greeting} world!`);
 
 [(1, 2, 3)].forEach(x => console.log(x));
