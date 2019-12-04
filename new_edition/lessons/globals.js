71b9e631cbc70e7dce797d10062e3debd0ca3941
sirwan <sir1afifi@gmail.com>
2019-12-05 00:53:07 +0330

    Provide helper functions as global

---
+++ new_edition/lessons/globals.js
@@ -0,0 +1,15 @@
+const { sumAsync, subtractAsync } = require("../math");
+
+let result, expected;
+
+test("sumAsync adds numbers", async () => {
+    result = await sumAsync(3, 7);
+    expected = 10;
+    expect(result).toBe(expected);
+});
+
+test("subtractAsync subtracts numbers", async () => {
+    result = await subtractAsync(7, 3);
+    expected = 4;
+    expect(result).toBe(expected);
+});
---
+++ new_edition/setup.globals.js
@@ -0,0 +1,24 @@
+async function test(title, callback) {
+    try {
+        await callback();
+        console.log(`✅ ${title}`);
+    } catch (error) {
+        console.error(`🔴 ${title}`);
+        console.error(error);
+    }
+}
+
+function expect(actual) {
+    return {
+        toBe(expected) {
+            if (actual !== expected) {
+                throw new Error(`${actual} is not equal to ${expected}`)
+            }
+        },
+        toEqual(expected) { },
+        toBeGreaterThan(expected) { }
+    }
+}
+
+global.test = test;
+global.expect = expect;
