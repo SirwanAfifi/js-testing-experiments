49fc3450f63372b22e8c863f44e2809b5b74c0b3
sirwan <sir1afifi@gmail.com>
2019-12-07 20:13:06 +0330

    Add original reference

--- new_edition/js-mocking-fundamentals/src/no-framework/monkey-patching.js
+++ new_edition/js-mocking-fundamentals/src/no-framework/monkey-patching.js
@@ -2,7 +2,11 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+const original = utils.getWinner;
+
 utils.getWinner = (p1, p2) => p1;
 
 const winner = thumbWar("Sirwan", "Shaho");
 assert.strictEqual(winner, "Sirwan");
+
+utils.getWinner = original;
