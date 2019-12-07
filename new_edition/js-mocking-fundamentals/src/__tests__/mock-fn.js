5ff8ea78e9e430b5f05f0399678c6fa6a9071a68
sirwan <sir1afifi@gmail.com>
2019-12-07 20:54:37 +0330

    using jest mock

--- new_edition/js-mocking-fundamentals/src/__tests__/mock-fn.js
+++ new_edition/js-mocking-fundamentals/src/__tests__/mock-fn.js
@@ -2,28 +2,20 @@ const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
 test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+  const originalGetWinner = utils.getWinner;
+  utils.getWinner = jest.fn((p1, p2) => p1);
 
-  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
-  expect(winner).toBe('Kent C. Dodds')
+  const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler');
+  expect(winner).toBe('Kent C. Dodds');
   expect(utils.getWinner.mock.calls).toEqual([
     ['Kent C. Dodds', 'Ken Wheeler'],
     ['Kent C. Dodds', 'Ken Wheeler']
-  ])
-  // could also do these assertions:
-  expect(utils.getWinner).toHaveBeenCalledTimes(2)
-  expect(utils.getWinner).toHaveBeenNthCalledWith(
-    1,
-    'Kent C. Dodds',
-    'Ken Wheeler'
-  )
-  expect(utils.getWinner).toHaveBeenNthCalledWith(
-    2,
-    'Kent C. Dodds',
-    'Ken Wheeler'
-  )
+  ]);
+  expect(utils.getWinner).toHaveBeenCalledTimes(2);
+  expect(utils.getWinner).toHaveBeenCalledWith('Kent C. Dodds', 'Ken Wheeler');
+  expect(utils.getWinner).toHaveBeenNthCalledWith(1, 'Kent C. Dodds', 'Ken Wheeler');
+  expect(utils.getWinner).toHaveBeenNthCalledWith(2, 'Kent C. Dodds', 'Ken Wheeler');
 
   // cleanup
-  utils.getWinner = originalGetWinner
+  utils.getWinner = originalGetWinner;
 })
